0e92c24dcc1ae64813ed02ed6daa01cc214a7ff0
ntthuan060102github <ntthuan060102.work@gmail.com>
2025-09-14 09:38:44 +0700

    update

--- learngaugeapis/models/exam.py
+++ learngaugeapis/models/exam.py
@@ -3,6 +3,7 @@ from django.db import models
 from learngaugeapis.const.exam_formats import ExamFormat
 from learngaugeapis.models.course_class import Class
 from learngaugeapis.models.clo_type import CLOType
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Exam(models.Model):
     id = models.AutoField(primary_key=True)
@@ -12,6 +13,7 @@ class Exam(models.Model):
     clo_type = models.ForeignKey(CLOType, on_delete=models.CASCADE, related_name='exams')
     exam_format = models.CharField(max_length=255, choices=ExamFormat.all())
     chapters = models.JSONField(default=list) # Example: [1, 2, 3]
+    pass_expectation_rate = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(null=True, default=None)
--- learngaugeapis/serializers/exam.py
+++ learngaugeapis/serializers/exam.py
@@ -5,12 +5,28 @@ from learngaugeapis.models.course_class import Class
 from learngaugeapis.models.exam import Exam
 from learngaugeapis.models.clo_type import CLOType
 from learngaugeapis.serializers.exam_result import ExamResultSerializer
+from learngaugeapis.serializers.course_class import ClassSerializer
+from learngaugeapis.serializers.course import CourseSerializer
+from learngaugeapis.serializers.major import MajorSerializer
+from learngaugeapis.serializers.clo_type import CLOTypeSerializer
+from learngaugeapis.serializers.academic_program import AcademicProgramSerializer
 
 class ExamSerializer(serializers.ModelSerializer):
     exam_results = ExamResultSerializer(many=True, read_only=True)
+    metadata = serializers.SerializerMethodField()
+
     class Meta:
         model = Exam
         fields = '__all__'
+
+    def get_metadata(self, obj: Exam):
+        return {
+            "course_class": ClassSerializer(obj.course_class).data,
+            "course": CourseSerializer(obj.course_class.course).data,
+            "major": MajorSerializer(obj.course_class.course.major).data,
+            "clo_type": CLOTypeSerializer(obj.clo_type).data,
+            "academic_program": AcademicProgramSerializer(obj.course_class.course.major.academic_program).data,
+        }
 
 class CreateExamSerializer(serializers.Serializer):
     course_class = serializers.PrimaryKeyRelatedField(queryset=Class.objects.filter(deleted_at=None))
@@ -19,7 +35,7 @@ class CreateExamSerializer(serializers.Serializer):
     clo_type = serializers.PrimaryKeyRelatedField(queryset=CLOType.objects.filter(deleted_at=None))
     exam_format = serializers.ChoiceField(choices=ExamFormat.all())
     chapters = serializers.ListField(child=serializers.IntegerField(min_value=1, max_value=100))
-    
+    pass_expectation_rate = serializers.IntegerField(min_value=0, max_value=100)
 class UpdateExamSerializer(serializers.Serializer):
     course_class = serializers.PrimaryKeyRelatedField(queryset=Class.objects.filter(deleted_at=None), required=False)
     name = serializers.CharField(required=False)
@@ -27,3 +43,4 @@ class UpdateExamSerializer(serializers.Serializer):
     clo_type = serializers.PrimaryKeyRelatedField(queryset=CLOType.objects.filter(deleted_at=None), required=False)
     exam_format = serializers.ChoiceField(choices=ExamFormat.all(), required=False)
     chapters = serializers.ListField(child=serializers.IntegerField(min_value=1, max_value=100), required=False)
+    pass_expectation_rate = serializers.IntegerField(min_value=0, max_value=100, required=False)
