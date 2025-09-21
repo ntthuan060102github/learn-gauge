3e41147f0952ac05a4c93a4b67670d83d42521c1
thuannt <thuannt@ghn.vn>
2025-09-21 11:21:36 +0700

    update

--- learngauge/settings.py
+++ learngauge/settings.py
@@ -237,6 +237,3 @@ AUTH_USER_MODEL = 'learngaugeapis.User'
 # Firebase Configuration
 FIREBASE_CERTIFICATE = config("FIREBASE_CERTIFICATE", "firebase_cert.json")
 FIREBASE_STORAGE_BUCKET_URL = config("FIREBASE_STORAGE_BUCKET_URL")
-
-PORT = config("PORT", 8000)
-print("PORT: ", PORT)
